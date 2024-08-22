1f3a3c32a49901437f1681efd6b009e02a7dfe2a
code- with-rajneesh <115329490+Rajneesh12345567@users.noreply.github.com>
2024-08-22 07:48:47 +0530

    Add files via upload

---
+++ ForTest.java
@@ -0,0 +1,10 @@
+class ForTest {
+    public static void main(String args[]) {
+        int x;
+        for (x = 0; x < 10; x = x + 1) {
+            System.out.println(" this is x:" + x);
+
+        }
+
+    }
+}
